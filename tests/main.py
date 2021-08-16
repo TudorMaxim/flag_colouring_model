527725bda8a61ef82bd00f993112c0f8859dd512
Tudor Maxim <maximtudor98@gmail.com>
2021-08-16 23:11:12 +0300

    testing utils package

--- tests/main.py
+++ tests/main.py
@@ -3,6 +3,7 @@ from model_tests import *
 from repository_tests import *
 from algorithms_tests import *
 from controller_tests import *
+from utils_tests import *
 
 if __name__ == '__main__':
     unittest.main()
